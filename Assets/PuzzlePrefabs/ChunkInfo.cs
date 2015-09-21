4368cd6ed8e7f7ad896a3b300e4b6dc064206dfe
BlueCrim <Blue.Crimson.Rage@gmail.com>
2015-09-22 09:50:06 +1000

    Added chunk info data to the prefabs

---
+++ Assets/PuzzlePrefabs/ChunkInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChunkInfo : MonoBehaviour {
+
+	public Rect chunkBounds;
+	public List<GameObject> exitPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
